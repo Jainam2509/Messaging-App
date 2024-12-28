588fde525d973aa993445bfc6bea1aefcb157159
Jainam2509 <jainampatel2509@gmail.com>
2024-12-28 14:19:38 +0530

    Update useAuthStore.js

--- frontend/src/store/useAuthStore.js
+++ frontend/src/store/useAuthStore.js
@@ -3,7 +3,7 @@ import { axiosInstance } from "../lib/axios.js";
 import toast from "react-hot-toast";
 import { io } from "socket.io-client";
 
-const BASE_URL = import.meta.env.MODE === "development" ? "http://localhost:5001" : "https://messaging-app-backend-nu.vercel.app/api";
+const BASE_URL = import.meta.env.MODE === "development" ? "http://localhost:5001" : "https://messaging-app-backend-nu.vercel.app";
 
 export const useAuthStore = create((set, get) => ({
   authUser: null,
