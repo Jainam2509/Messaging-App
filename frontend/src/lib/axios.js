78cb88eca7ce53a1b47f5a3e4518f366a3a7c2f7
Jainam2509 <jainampatel2509@gmail.com>
2024-12-31 00:55:33 +0530

    Update axios.js

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
 export const axiosInstance = axios.create({
-  baseURL: "https://www.matangievent.com",
+  baseURL: "https://www.matangievent.com/api",
   withCredentials: true,
 });
