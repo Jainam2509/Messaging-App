1a3c36c225572efd67a8b3d900021857cf4e2b43
Jainam2509 <jainampatel2509@gmail.com>
2024-12-31 00:22:23 +0530

    Updated

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
 export const axiosInstance = axios.create({
-  baseURL: "https://messaging-app-j3o4.onrender.com/api",
+  baseURL: "www.matangievent.com/api",
   withCredentials: true,
 });
